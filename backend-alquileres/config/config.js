26b670c1488ee3e2a65014f35640c979163874d6
Kira <josemarinramos100@gmail.com>
2025-06-04 22:55:42 -0500

    feat(auth): implement refresh token management with storage, verification, and logout functionality

--- backend-alquileres/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secretKey: process.env.JWT_SECRET || 'tu_clave_secreta_temporal',
-  // Aquí puedes agregar más configuraciones según sea necesario
-}; 
--- backend-alquileres/middlewares/rolMiddleware.js
+++
@@ -1,9 +0,0 @@
-const verificarRol = (rolesPermitidos) => (req, res, next) => {
-    if (!rolesPermitidos.includes(req.usuario.rol)) {
-        return res.status(403).json({ mensaje: 'Acceso denegado. Rol no autorizado.' });
-    }
-    next();
-};
-
-
-module.exports = verificarRol;
